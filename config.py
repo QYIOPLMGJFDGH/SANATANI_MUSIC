d59b7edab6fd50adddae63cda2b3375cc6db2b12
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-07 23:35:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ________________________________________________________________________________#
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002043570167"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002043570167"))
 # ________________________________________________________________________________#
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
